0fdb259c64596334f6952c142fada3c093067cce
Clay <clay@clay.rip>
2026-04-02 17:04:01 -0400

    fix

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,18 +1,16 @@
-window.addEventListener('load', function () {
-    let fakeDay = Math.floor(Math.random() * 10000) + 1000;
+let fakeDay = Math.floor(Math.random() * 10000) + 1000;
 
-    const nativeUTC = Date.UTC;
-    Date.UTC = function(year, month, day) {
-        if (arguments.length === 3) {
-            if (arguments[1] === 0 && arguments[2] === 0) {
-                return 0; 
-            }
-            return fakeDay * 86400000;
+const nativeUTC = Date.UTC;
+Date.UTC = function(year, month, day) {
+    if (arguments.length === 3) {
+        if (arguments[1] === 0 && arguments[2] === 0) {
+            return 0; 
         }
-        return nativeUTC.apply(this, arguments);
-    };
+        return fakeDay * 86400000;
+    }
+    return nativeUTC.apply(this, arguments);
+};
 
-    setInterval(() => {
-        fakeDay++;
-    }, 2000);
-});
+setInterval(() => {
+    fakeDay++;
+}, 2000);
