bdad5091df7e14769c2645dfab45b788949a7e59
Clay <claytontdm@gmail.com>
2026-04-02 16:41:23 -0400

    infinite saves without reloads!!!

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,26 +1,18 @@
-localStorage.setItem('user-settings', '{"lastNewsCheck":"1970-01-01T00:00:00Z"}');
-let deletedModal = false;
-window.addEventListener('load', function () {
-    /*
-    let targetNode = document.body;
-    let config = { childList: true, subtree: true };
-    let observer = new MutationObserver(function (mutationsList, observer) {
-        for (let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                let modal = document.getElementsByClassName('modal')[0];
-                if (modal && !deletedModal) {
-                    setTimeout(() => {
-                        modal.remove();
-                        deletedModal = true;
-                    }, 50);
-                }
+(function() {
+    let fakeDay = Math.floor(Math.random() * 10000) + 1000;
+
+    const nativeUTC = Date.UTC;
+    Date.UTC = function(year, month, day) {
+        if (arguments.length === 3) {
+            if (arguments[1] === 0 && arguments[2] === 0) {
+                return 0; 
             }
+            return fakeDay * 86400000;
         }
-    });
-    observer.observe(targetNode, config);
-    */
+        return nativeUTC.apply(this, arguments);
+    };
+
     setInterval(() => {
-        console.log('resetting daily saves')
-        localStorage.setItem('user-settings', `{"lastNewsCheck":"${new Date().toISOString()}"}`);
-    }, 500);
-});
+        fakeDay++;
+    }, 2000);
+})();
